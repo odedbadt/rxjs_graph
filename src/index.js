6b0ff5ef4bb76114897d9ed48e26f2559d83bb75
Oded Network <obadt@infinidat.com>
2020-12-17 15:54:49 +0200

    Backup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { fromEvent, merge, of} from 'rxjs';
 import { scan, map as rxMap, mergeMap } from 'rxjs/operators';
 import { partial, defaults, forEach, map, random, findIndex, range} from 'lodash-es';
+import { collectionData } from 'rxfire/firestore';
 
 function norm2(x,y) {
   return x*x + y*y;
@@ -46,7 +47,7 @@ function render(ctx, state) {
   forEach(state.circles, function(c) {
     ctx.beginPath();
     ctx.arc(c[0][0], c[0][1], c[1], 0, 2 * Math.PI);
-    ctx.fillStyle= 'red'
+    ctx.fillStyle= 'blue'
     ctx.fill();
   });
   forEach(state.edges, function(e) {
@@ -64,22 +65,164 @@ function render(ctx, state) {
 function init() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    const down = fromEvent(document, 'mousedown');
-    const move = fromEvent(document, 'mousemove');
-    const up = fromEvent(document, 'mouseup');
     const mouse = of('mousedown', 'mousemove', 'mouseup').pipe(
       mergeMap(partial(fromEvent,document)));
+    const graphData = app.firestore().collection('graphs');
+    graphData.where('name', '==', 'initial');
+    graphChanges$ = collectionData(graphData, 'id')
+    graphChanges$.tap(console.log)
     const initial_state = {
       'chosen_circle': -1,
       'circles': map(range(0,41), x=>[
         [random(10,590),random(10,590)],
         10]),
-      'edges': map(range(0,500), x=>
+      'edges': map(range(0,50), x=>
         [random(0,40), random(0,40)])
     }
     const mouse_observer = mouse.pipe(scan(process_mouse, initial_state))
     render(ctx, initial_state);
-    mouse_observer.subscribe(partial(render, ctx))
+    mouse_observer.subscribe(partial(render, ctx));
+
 }
+// Loads chat messages history and listens for upcoming ones.
+function loadMessages() {
+  // Create the query to load the last 12 messages and listen for new ones.
+  var query = firebase.firestore()
+                  .collection('messages')
+                  .orderBy('timestamp', 'desc')
+                  .limit(12);
+
+  // Start listening to the query.
+  query.onSnapshot(function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name,
+                       message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
+}
+// Signs-in Graph.
+function signIn() {
+  // Sign into Firebase using popup auth & Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
+// Signs-out of Graph.
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
+// Initiate Firebase Auth.
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+// Returns the signed-in user's display name.
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) { // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameElement.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    userNameElement.removeAttribute('hidden');
+    userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+
+    // We save the Firebase Messaging Device token and enable notifications.
+    saveMessagingDeviceToken();
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    userNameElement.setAttribute('hidden', 'true');
+    userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+// Returns true if user is signed-in. Otherwise false and displays a messages.
+function checkSignedInWithMessage() {
+  // Return true if the user is signed in Firebase
+  if (isUserSignedIn()) {
+    return true;
+  }
+
+  // Display a message to the user using a Toast.
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000
+  };
+  signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
+  return false;
+}
+
+// Resets the given MaterialTextField.
+function resetMaterialTextfield(element) {
+  element.value = '';
+  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+}
+
+// Template for messages.
+var MESSAGE_TEMPLATE =
+    '<div class="message-container">' +
+      '<div class="spacing"><div class="pic"></div></div>' +
+      '<div class="message"></div>' +
+      '<div class="name"></div>' +
+    '</div>';
+
+// Adds a size to Google Profile pics URLs.
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+function checkSetup() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+}
+
+// Checks that Firebase has been imported.
+checkSetup();
+
+// Shortcuts to DOM Elements.
+var signInButtonElement = document.getElementById('sign-in');
+var signOutButtonElement = document.getElementById('sign-out');
+var signInSnackbarElement = document.getElementById('must-signin-snackbar');
+
+// Saves message on form submit.
+signOutButtonElement.addEventListener('click', signOut);
+signInButtonElement.addEventListener('click', signIn);
+
+initFirebaseAuth();
+
 
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
-var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 var webpack = require('webpack');
 module.exports = {
   'entry': './src/index.js',
   'watch':true,
-  'devtool': "eval-source-map"
+  'devtool': "eval-source-map",
+   output: {
+    filename: 'main.js',
+    path: __dirname + '/public'
+  }
 };
